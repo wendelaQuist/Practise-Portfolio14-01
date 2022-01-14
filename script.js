c7add5defaa3828d193bf4bf15fb56db13e9aaab
wendelaQuist <wendela140200@gmail.com>
2022-01-14 19:00:54 +0100

    custom cursor and changed layout

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
@@ -10,3 +12,21 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
+
+window.onmousemove = (e) => {
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links => {
+    links.onmouseenter = () => {
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+    links.onmouseleave = () => {
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+});
